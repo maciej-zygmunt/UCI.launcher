ab14334b58e18ecbce2eb258a1a027ff295fd0ad
mzygmunt <mzygmunt@agh.edu.pl>
2019-07-03 12:00:14 +0200

    Initial commit

---
+++ src/uci/launcher/Perspective.java
@@ -0,0 +1,27 @@
+package uci.launcher;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	/**
+	 * The ID of the perspective as specified in the extension.
+	 */
+	public static final String ID = "UCI.launcher.perspective";
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(false);
+		
+		layout.addStandaloneView("com.eclipserunner.views.RunnerView",  false, IPageLayout.LEFT, 0.25f, editorArea);
+		/*
+		IFolderLayout folder = layout.createFolder("messages", IPageLayout.TOP, 0.5f, editorArea);
+		folder.addPlaceholder(View.ID + ":*");
+		folder.addView(View.ID);
+		
+		layout.getViewLayout(NavigationView.ID).setCloseable(false);
+		*/
+	}
+}
